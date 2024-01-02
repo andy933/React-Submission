54daf0aa05740247cd6bea45be60aa2d6c31372a
Vict101 <yngai8-c@my.cityu.edu.hk>
2024-01-02 16:23:48 +0800

    put

--- app/index.js
+++ app/index.js
@@ -2,36 +2,73 @@ require('dotenv').config()
 const express = require('express')
 const app = express()
 const Note = require('./models/note')
-
+// ------------------------------ all middlewares should be first except the error one
 const cors = require('cors')
 app.use(cors())
 app.use(express.static('dist'))
+app.use(express.json())
 
+const requestLogger = (request, response, next) => {
+  console.log('Method:', request.method)
+  console.log('Path:  ', request.path)
+  console.log('Body:  ', request.body)
+  console.log('---')
+  next()
+}
+
+app.use(requestLogger)
+// ----------------------------------------
 app.get('/', (request, response) => {
   response.send('<h1>Hello World!</h1>')
 })
 
 app.get('/api/notes', (request, response) => {
-  //response.json(notes)
   Note.find({}).then(notes => {
     response.json(notes)
   })
 })
 
-app.get('/api/notes/:id', (request, response) => {
+app.get('/api/notes/:id', (request, response, next) => {
   Note.findById(request.params.id).then(note => {
-    response.json(note)
+    if (note) {
+      response.json(note)
+    }
+    else {
+      response.status(404).end()
+    }
   })
+  .catch(error => next(error))
+// input a wrong id format
+/*   .catch(error => {
+    console.log(error)
+    response.status(400).send({ error: 'malformatted id' })
+  }) */
+// 500: internal server error
+/*   .catch(error => {
+    console.log(error)
+    response.status(500).end()
+  }) */
 })
 
-/* app.delete('/api/notes/:id', (request, response) => {
-  const id = Number(request.params.id)
-  notes = notes.filter(note => note.id !== id)
+app.delete('/api/notes/:id', (request, response, next) => {
+  Note.findByIdAndDelete(request.params.id).then(results => {
+    response.status(204).end()
+  })
+  .catch(error => next(error))
+})
 
-  response.status(204).end()
-}) */
+app.put('/api/notes/:id', (request, response, next) => {
+  const body = request.body
+  const note = {
+    content: body.content,
+    important: body.important
+  }
+  Note.findByIdAndUpdate(request.params.id, note, { new: true }).then(updatedNote => {
+    response.json(updatedNote)
+  })
+  .catch(error => next(error))
+})
   
-app.use(express.json())
 app.post('/api/notes', (request, response) => {
   const body = request.body
   
@@ -51,21 +88,23 @@ app.post('/api/notes', (request, response) => {
   })
 })
 
-const requestLogger = (request, response, next) => {
-  console.log('Method:', request.method)
-  console.log('Path:  ', request.path)
-  console.log('Body:  ', request.body)
-  console.log('---')
-  next()
-}
-
-app.use(requestLogger)
-
 const unknownEndpoint = (request, response) => {
   response.status(404).send({ error: 'unknown endpoint' })
 }
 
 app.use(unknownEndpoint)
+// the error-handling middleware has to be the last loaded middleware!
+const errorHandler = (error, request, response, next) => {
+  console.error(error.message)
+
+  if (error.name === 'CastError') {
+    return response.status(400).send({ error: 'malformatted id' })
+  }
+
+  next(error)
+}
+
+app.use(errorHandler)
 
 const PORT = process.env.PORT
 app.listen(PORT, () => {
