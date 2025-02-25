acf3d4f17a9e84f47da9bd28c679c09402c8c7f6
andy.ngai <andy.ngai@itchannel.com.hk>
2025-02-25 16:34:59 +0800

    part 5 branch push

--- part2/phonebook - frontend/src/components/Persons.jsx
+++
@@ -1,9 +0,0 @@
-const Persons = ({ persons, filterName, deletePersonOf }) => {
-    return (
-        <>
-            <p>{persons.name} {persons.number} <button onClick={deletePersonOf}>delete</button></p>
-        </>
-    )
-}
-
-export default Persons
