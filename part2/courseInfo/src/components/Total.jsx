e7ae5de666440b0b26b49d850e9500ef4738effa
andy.ngai <andy.ngai@itchannel.com.hk>
2025-02-25 16:25:44 +0800

    test

--- part2/courseInfo/src/components/Total.jsx
+++
@@ -1,3 +0,0 @@
-const Total = ({ sum }) => <p> <b>total of exercises {sum}</b></p>
-
-export default Total
--- part2/phonebook - frontend/src/components/Filter.jsx
+++
@@ -1,9 +0,0 @@
-const Filter = ({ filterName, handleFilterChange }) => {
-    return (
-      <div>
-        filter shown with <input value={filterName} onChange={handleFilterChange} />
-      </div>
-    )
-  }
-  
-  export default Filter
